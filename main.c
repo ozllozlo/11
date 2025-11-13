dac5f84871cd7ff47458ef4bdbc2c6668fa2dc87
ozllozlo <ozllozlo@sookmyung.ac.kr>
2025-11-13 15:54:51 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+char*proverb="All that glisters is not gold.";
+void setPointer(char*q)
 {
-     int i=100;
-     int *p=&i;
-     int **q=&p;
-     
-    *p=200;
-    printf("i=%d, *p=%d, **q=%d\n", i, *p,**q);
-    
-    **q=300;
-    printf("i=%d, *p=%d, **q=%d\n", i, *p,**q);
+     q=proverb;
+     }
+     int main(void)
+     {
+         char*p="zzz";
+         setPointer(p);
+         printf("%s\n", p);
+         
    
   system("PAUSE");	
   return 0;
